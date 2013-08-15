d3cf1473a646f1bc013d02e70d4faa948ce1f671
ReWiG <kirundel963@gmail.com>
2013-08-15 14:44:54 +0400

    Класс контроллер, коннект

---
+++ MetallDon Controller Manager/Controller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using MOXA_CSharp_MXIO;
+
+namespace MetallDon_Controller_Manager
+{
+    class Controller
+    {
+        int ret;
+        Int32[] hConnection = new Int32[1];
+        string IPAddr;
+        ushort port = 502;
+        string Password = "";
+        UInt32 Timeout = 5000;
+        DateTime lastUpdatetime;
+        Timer timer = new Timer();
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="ip">IP-адрес контроллера</param>
+        /// <param name="pswrd">Пароль</param>
+        /// <param name="timeout">Время таймаута, мс</param>
+        /// <param name="ping">Интервал опроса, секунды</param>
+        /// <param name="updateTime">Время последнего обновления параметров из базы</param>
+        public Controller(String ip, String pswrd, UInt32 timeout, Int32 ping, DateTime updateTime)
+        {
+            ret = MXIO_CS.MXEIO_Init();
+            CheckErr(ret, "MXEIO_Init");
+            timer.Interval = ping;
+            IPAddr = ip;
+            Password = pswrd;
+            Timeout = timeout;
+            lastUpdatetime = updateTime;
+
+            Console.WriteLine( Connect());
+            Console.ReadKey();
+        }
+
+        public Boolean Connect() {
+            Console.WriteLine("Подключение к IP={0}, Порт={1} Таймаут={2}, Пароль={3}", IPAddr, port, Timeout, Password);
+            ret = MXIO_CS.MXEIO_E1K_Connect(System.Text.Encoding.UTF8.GetBytes(IPAddr), port, Timeout, hConnection, System.Text.Encoding.UTF8.GetBytes(Password));
+            return CheckErr(ret, "Connect");
+        }
+
+        public static Boolean CheckErr(int iRet, string functionName)
+        {
+            Console.WriteLine("Функция \"{0}\". Сообщение : {1}\n", functionName, Enum.GetName(typeof(MXIO_CS.MXIO_ErrorCode), iRet));
+            if (iRet == MXIO_CS.EIO_TIME_OUT || iRet == MXIO_CS.HANDLE_ERROR)
+            {
+                //Завершаем работу
+                //MXIO_CS.MXEIO_Exit();
+                //Console.WriteLine("Нажмите любую клавишу для выхода из программы\r\n");
+                //Console.ReadLine();
+                //Environment.Exit(1);
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- MetallDon Controller Manager/Program.cs
+++ MetallDon Controller Manager/Program.cs
@@ -9,14 +9,64 @@ namespace MetallDon_Controller_Manager
 {
     class Program
     {
+        public static void CheckErr(int iRet, string szFunctionName)
+        {
+            Console.WriteLine("Function \"{0}\". Return Message : {1}\n", szFunctionName, Enum.GetName(typeof(MXIO_CS.MXIO_ErrorCode), iRet));
+            if (iRet == MXIO_CS.EIO_TIME_OUT || iRet == MXIO_CS.HANDLE_ERROR)
+            {
+                //To terminates use of the socket
+                MXIO_CS.MXEIO_Exit();
+                Console.WriteLine("Press any key to close application\r\n");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+        }
+
         static void Main(string[] args)
         {
-            int iii = MXIO_CS.MXEIO_Init();
-            Console.WriteLine("привет");
-            Console.WriteLine(iii);
+            Controller contr = new Controller("192.168.127.254","",3000,20,new DateTime());
+            ////Console.WriteLine(MetallDon_Controller_Manager.Properties.Settings.Default.ipDB);
+            //int iii = MXIO_CS.MXEIO_Init();
+            //CheckErr(iii, "init");
 
-            Console.ReadKey();
-            MXIO_CS.MXEIO_Exit();
+            //int ret;
+            //Int32[] hConnection = new Int32[1];
+            //string IPAddr = "192.168.127.254";
+            //string Password = "";
+            //UInt32 Timeout = 5000;
+
+            ////Connect to ioLogik device
+            //Console.WriteLine("MXEIO_E1K_Connect IP={0}, Timeout={1}, Password={2}", IPAddr, Timeout, Password);
+            //ret = MXIO_CS.MXEIO_E1K_Connect(System.Text.Encoding.UTF8.GetBytes(IPAddr), 502, Timeout, hConnection, System.Text.Encoding.UTF8.GetBytes(Password));
+            //CheckErr(ret, "MXEIO_E1K_Connect");
+            //if (ret == MXIO_CS.MXIO_OK)
+            //    Console.WriteLine("MXEIO_E1K_Connect Success.");
+            ////------------------------------------------------------------------------
+            //byte bytCount = 16;
+            //byte bytStartChannel = 0;
+            //Int32 dwShiftValue;
+            //UInt32 i;
+            //UInt32[] dwGetDIValue = new UInt32[1];
+            //ret = MXIO_CS.E1K_DI_Reads(hConnection[0], bytStartChannel, bytCount, dwGetDIValue);
+            //CheckErr(ret, "E1K_DI_Reads");
+            //if (ret == MXIO_CS.MXIO_OK)
+            //{
+            //    Console.WriteLine("E1K_DI_Reads Get Ch0~ch3 DI Direction DI Mode DI Value success.");
+            //    for (i = 0, dwShiftValue = 0; i < bytCount; i++, dwShiftValue++)
+            //        Console.WriteLine("DI vlaue: ch[{0}] = {1}", i + bytStartChannel, ((dwGetDIValue[0] & (1 << dwShiftValue)) == 0) ? "OFF" : "ON");
+            //}
+            ////--------------------------------------------------------------------------
+            ////End Application
+            //ret = MXIO_CS.MXEIO_Disconnect(hConnection[0]);
+            //CheckErr(ret, "MXEIO_E1K_Connect");
+            //if (ret == MXIO_CS.MXIO_OK)
+            //    Console.WriteLine("MXEIO_Disconnect return {0}", ret);
+            ////--------------------------------------------------------------------------
+            //MXIO_CS.MXEIO_Exit();
+            //Console.WriteLine("MXEIO_Exit, Press Enter To Exit.");
+            //Console.ReadLine();
+
+            //Console.ReadKey();
         }
     }
 }
